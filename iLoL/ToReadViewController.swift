ee9e0c0815309a8aaf97c230bd57ea36ffab13d5
alwongg <wong_1994@hotmail.com>
2017-08-13 15:45:49 -0500

    Added ToRead page

---
+++ iLoL/ToReadViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ToReadViewController.swift
+//  iLoL
+//
+//  Created by Alex Wong on 8/13/17.
+//  Copyright © 2017 Alex Wong. All rights reserved.
+//
+
+import UIKit
+
+class ToReadViewController: UIViewController {
+
+    @IBAction func cancel(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+
+}
